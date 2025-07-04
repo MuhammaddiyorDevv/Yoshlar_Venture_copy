e18d9ee5853e13793917dd3460c49689d3f3bed4
Muhammadrasul21 <theboy6878@gmail.com>
2025-07-04 23:29:46 +0500

    restored codes

--- src/components/ToggleTheme.jsx
+++ src/components/ToggleTheme.jsx
@@ -13,5 +13,12 @@ export default function ToggleTheme() {
     localStorage.setItem("theme", isDark ? "dark" : "light");
   };
 
-  return null
+  return (
+    <button
+      onClick={toggleDarkMode}
+      className="px-4 py-2 rounded bg-gray-800 text-white dark:bg-yellow-300 dark:text-black transition"
+    >
+      Dark Mode
+    </button>
+  );
 }
